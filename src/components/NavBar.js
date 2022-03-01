ee34b0cf82dc7348de5528ce02b1f4297015f200
Florencia <fdubininkas@gmail.com>
2022-03-01 01:24:02 -0300

    Cambios en NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
-import logo from "../assets/img/coffee-plant.png";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import logo from "../assets/img/logo.png";
 
 const NavBar = () => {
     return (
         <Navbar bg="light" expand="lg">
-            <Container>
+            <Container fluid>
                 <Navbar.Brand href="#home">
-                    OH MY COFFEE
                 <img alt="plant" src={logo} className="d-inline-block align-top logo" />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto" >
-                        <Nav.Link href="#home">Inicio</Nav.Link>
+                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                    <Nav >
+                        <Nav.Link href="#link">Inicio</Nav.Link>
                         <Nav.Link href="#link">Productos</Nav.Link>
-                        <NavDropdown title="Otros" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Sucursales</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Sobre Nosotros</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Contacto</NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="#link">Contacto</Nav.Link>
+                        <Nav.Link href="#link">Carrito</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
